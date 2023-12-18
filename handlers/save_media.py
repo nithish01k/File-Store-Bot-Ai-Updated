6a614e63a7297b6f446a303d17cd9dfff30ce5f6
Sonali Saha <152001935+sonali1563@users.noreply.github.com>
2023-12-18 20:06:42 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -67,12 +67,12 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
         share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=missqueenbotx_{str_to_b64(str(SaveMessage.id))}"
         short_link = get_short(share_link)
         await editable.edit(
-            #f"**<b>🍁 Batch Files Stored in my Database!**\n\n❕ Hᴇʀᴇ ɪs ᴛʜᴇ Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ ᴏғ ʏᴏᴜʀ ғɪʟᴇs: <code>{share_link}</code> \n\n"
+            f"**<b>🍁 Batch Files Stored in my Database!**\n\n⭐ Hᴇʀᴇ ɪs ᴛʜᴇ Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ ᴏғ ʏᴏᴜʀ ғɪʟᴇs: <code>{share_link}</code> \n\n"
             f"**<b>🍁 Batch Files Stored in my Database!**\n\n⭐ Hᴇʀᴇ ɪs ᴛʜᴇ Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ ᴏғ ʏᴏᴜʀ ғɪʟᴇs: <code>{short_link}</code> \n\n"
             f"Just Click the link to get your files! </b>",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("🍁 Original Link 🍁", url=short_link),
-                  InlineKeyboardButton("🍁 How To Open 🍁", url="https://t.me/hindi_movies_free_downlo/456")]]
+                [[InlineKeyboardButton("🍁 Original Link 🍁", url=share_link),
+                  InlineKeyboardButton("🌿 Support Link 🌿", url=short_link)]]
             ),
             disable_web_page_preview=True
         )
