884a459a5ecce7b6d94e5a299840e199b8df6f55
Sonali Saha <152001935+sonali1563@users.noreply.github.com>
2023-12-25 18:39:50 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -68,8 +68,8 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
         short_link = get_short(share_link)
         await editable.edit(
             f"**<b>🍁 Batch Files Stored in my Database!**\n\n⭐ Hᴇʀᴇ ɪs ᴛʜᴇ Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ ᴏғ ʏᴏᴜʀ ғɪʟᴇs: <code>{share_link}</code> </b>\n\n"
-            f"**<b>⭐ Hᴇʀᴇ ɪs ᴛʜᴇ Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ ᴏғ ʏᴏᴜʀ ғɪʟᴇs: <code>{short_link}</code> \n\n"
-            f"Just Click the link to get your files! </b>",
+            # f"**<b>⭐ Hᴇʀᴇ ɪs ᴛʜᴇ Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ ᴏғ ʏᴏᴜʀ ғɪʟᴇs: <code>{short_link}</code> \n\n"
+            f"Just Click the link to get your files! 👆🏻</b>",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("🍁 Original Link 🍁", url=share_link),
                   InlineKeyboardButton("Support Link ✨", url=short_link)]]
